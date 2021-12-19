a3fdd9ce02d575934584469bfe6c9a73e9af05f7
Joe <palmer-2@hotmail.co.uk>
2021-12-19 14:52:02 +0000

    day 15 solution needs a newer search method

--- day15.py
+++ day15.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-# SHOULD WORK BUT SAYS i HAVE THE WRONG INPUT???
+# Doesn't work. Needs something more complex. Implement an A* algorithm or graph
+# search
 def getminpath(data):
   cost = np.full(data.shape, np.nan)
   first_row = data[0,:]
@@ -15,7 +16,7 @@ def getminpath(data):
     for j in range(1, cost.shape[-1]):
       cost[i, j] = data[i, j] + min(cost[i-1, j], cost[i, j-1])
 
-  answer = cost[data.shape[0]-1, data.shape[-1]-1] - data[0, 0]
+  answer = cost[data.shape[0]-1, data.shape[-1]-1] #- data[0, 0]
   return answer
 
 path = "data/day15_data.txt"
